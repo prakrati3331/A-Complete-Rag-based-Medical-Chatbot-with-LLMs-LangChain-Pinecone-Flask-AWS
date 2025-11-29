a6df4f121cd7f008eb553fa47967bd775e8db1b8
prakrati3331 <prakratimishra2006@gmail.com>
2025-11-29 18:39:02 +0530

    Initial commit: Medical Chatbot with LLMs, LangChain, Pinecone, Flask, and AWS

---
+++ setup.py
@@ -0,0 +1,11 @@
+from gettext import install
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Prakrati Mishra",
+    author_email="prakratimishra2006@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
